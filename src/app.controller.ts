e883b151dfb38c1197bdb3fc6bce6e6035f34da7
mohdbinharun56 <arahaman461@gmail.com>
2024-11-05 18:42:08 +0600

    implement async await and promise programming

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 @Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  // Get method
   @Get()
   getHello(): string {
     // return "All the users";
     return this.appService.getHello();
   }
+
+  // Async Await and Promise
+  @Get('async')
+  async getAsync():Promise<number>{
+    return this.appService.getAsync();
+  }
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World NEST JS!';
   }
+
+  async getAsync():Promise<number>{
+    const a = 3;
+    const b =1;
+    const c = await this.add(a,b); 
+    return c;
+  }
+
+  async add(a:number,b:number):Promise<number>{
+    return a+b;
+  }
 }
