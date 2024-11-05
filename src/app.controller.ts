968edc451e91ced59b228f64a218e63029a8a90f
mohdbinharun56 <arahaman461@gmail.com>
2024-11-05 23:18:19 +0600

    added catDTO file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { ConsoleLogger, Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('user')
---
+++ src/app.createCatDTO.ts
@@ -0,0 +1,5 @@
+export class CreatCatDTO{
+    name: string;
+    age: number;
+    breed: string;
+}
